a56212c245455a59b19059e69c1feede3bcd9e37
nahoe Kim <sukjinjin921204@gmail.com>
2026-09-21 23:56:54 +0900

    Create HW1_1_1.c

---
+++ HW_Stack/HW1_1_1.c
@@ -0,0 +1,69 @@
+//stack에서 숫자 삽입할 때(입력받는 값X, 입력되어 있을 때)
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAX_STACK_SIZE 100 // 배열을 이용한 스택은 사이즈의 제한이 있다
+// 스택을 위한 타입 정의
+typedef int element;
+typedef struct {
+	element data[MAX_STACK_SIZE];
+	int top;
+} StackType;
+// 스택 초기화 함수
+void init(StackType* s)
+{
+	s->top = -1;
+}
+// 공백 상태 검출 함수
+int is_empty(StackType* s)
+{
+	return (s->top == -1);
+}
+// 포화 상태 검출 함수
+int is_full(StackType* s)
+{
+	return (s->top == (MAX_STACK_SIZE - 1));
+}
+// 삽입함수
+void push(StackType* s, element item)
+{
+	if (is_full(s)) {
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+	}
+	else
+		s->data[++(s->top)] = item;
+}
+// 삭제함수
+element pop(StackType* s)
+{
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else
+		return s->data[(s->top)--];
+}
+// 피크함수
+element peek(StackType* s)
+{
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else
+		return s->data[s->top];
+}
+// 주함수
+void main()
+{
+	StackType s;
+	int num;
+	init(&s);
+	push(&s, 10);
+	push(&s, 20);
+	push(&s, 30);
+	printf("%d\n", pop(&s));
+	printf("%d\n", pop(&s));
+	printf("%d\n", pop(&s));
+	//printf("%d\n", is_empty(&s));
+}
